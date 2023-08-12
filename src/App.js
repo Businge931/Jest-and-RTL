e316c39535c9e8d7e4512d42ee3edd3432af9976
Businge931 <jimmybusinge9@gmail.com>
2023-08-13 00:47:39 +0300

    feat: code  checkbox functionality

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import "./App.css";
 
 function App() {
   const [buttonColor, setButtonColor] = useState("red");
+  const [isDisabled, setIsDisabled] = useState(false);
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
   return (
     <div className="App">
       <button
         onClick={() => setButtonColor(newButtonColor)}
         style={{ backgroundColor: buttonColor }}
+        disabled={isDisabled}
       >
         Change to {newButtonColor}
       </button>
+      <input
+        type="checkbox"
+        onChange={(e) => setIsDisabled(e.target.checked)}
+        aria-checked={isDisabled}
+        defaultChecked={isDisabled}
+      />
     </div>
   );
 }
