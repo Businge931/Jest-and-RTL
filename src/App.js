485e00d834f8a894568d20f40f8ae1b579865285
Businge931 <jimmybusinge9@gmail.com>
2023-08-13 00:27:11 +0300

    feat:write jsx for tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => setButtonColor(newButtonColor)}
+        style={{ backgroundColor: buttonColor }}
+      >
+        Change to {newButtonColor}
+      </button>
     </div>
   );
 }
